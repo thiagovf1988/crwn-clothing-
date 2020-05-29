ae8283f28e127525ff0945b4227167a7796e3177
thiagovf1988 <61379143+thiagovf1988@users.noreply.github.com>
2020-05-29 18:08:32 +0100

    new store project

---
+++ src/homepage.component.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <h1> ggg</h1>
+        {/* function component..dont need lifecyclie for now */}
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        Hats
+                </div>
+                    <span className='subtitle'> shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        Hats
+                </div>
+                    <span className='subtitle'> shop now</span>
+                </div>
+            </div>
+
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        Hats
+                </div>
+                    <span className='subtitle'> shop now</span>
+                </div>
+            </div>
+
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        Hats
+                </div>
+                    <span className='subtitle'> shop now</span>
+                </div>
+            </div>
+
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>
+                        Hatsf
+                    </h1>
+                    <span className='subtitle'> shop now</span>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+);
+export default HomePage;
