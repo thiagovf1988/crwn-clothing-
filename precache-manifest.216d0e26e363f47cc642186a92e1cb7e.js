190878c156fd18e28f62a74df1e3ba57eee46ef6
thiagovf1988 <61379143+thiagovf1988@users.noreply.github.com>
2020-05-30 00:22:24 +0100

    Updates

---
+++ precache-manifest.216d0e26e363f47cc642186a92e1cb7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4f7f0f2fc8fa4b1d5751ffc50da16d",
+    "url": "/crwn-clothing-/index.html"
+  },
+  {
+    "revision": "b4e4dba8ae9d5d95613e",
+    "url": "/crwn-clothing-/static/css/main.5f965fba.chunk.css"
+  },
+  {
+    "revision": "1b717a6709ba9a0edd98",
+    "url": "/crwn-clothing-/static/js/2.6e7c6510.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crwn-clothing-/static/js/2.6e7c6510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e4dba8ae9d5d95613e",
+    "url": "/crwn-clothing-/static/js/main.fa10c818.chunk.js"
+  },
+  {
+    "revision": "9774d0c1137ec7a46d13",
+    "url": "/crwn-clothing-/static/js/runtime-main.14b0fc83.js"
+  }
+]);
